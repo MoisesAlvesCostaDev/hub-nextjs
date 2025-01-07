2ed70561677ec8585ad9cdfb1c5fbbcf3d9f3969
Moisés Alves Costa <moi.ac@hotmail.com>
2025-01-06 21:28:39 -0300

    feat: install material ui

---
+++ theme.ts
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#dc004e",
+    },
+    background: {
+      default: "#f5f5f5",
+    },
+    text: {
+      primary: "#333333",
+      secondary: "#707070",
+    },
+  },
+});
+
+export default theme;
